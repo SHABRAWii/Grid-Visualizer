eca317c0897dcf9d35c25169d64dd13b64fec5aa
Khaled <khaled.abdelaziz@bld.ai>
2023-01-13 22:13:36 +0200

    Adding Grid Render

--- src/Custom.c
+++ src/Custom.c
@@ -1,36 +1,42 @@
-#include <stdio.h>
-#include <stdbool.h>
-#include <SDL2/SDL.h>
 #include "Custom.h"
 
+#include <SDL2/SDL.h>
+#include <stdbool.h>
+#include <stdio.h>
 
 // Define MAX and MIN macros
 #define MAX(X, Y) (((X) > (Y)) ? (X) : (Y))
 #define MIN(X, Y) (((X) < (Y)) ? (X) : (Y))
 
 // Define screen dimensions
-#define SCREEN_WIDTH    800
-#define SCREEN_HEIGHT   600
+#define SCREEN_WIDTH 800
+#define SCREEN_HEIGHT 600
 
-int Draw(){
+int DrawGrid(int rows, int columns, SDL_Window *window, SDL_Renderer *rendrer) {
+    // Declare rect of square
+    SDL_Rect squareRect;
 
-    printf("Hello World\n");
+    // Square dimensions: Half of the min(SCREEN_WIDTH, SCREEN_HEIGHT)
+    squareRect.w = MIN(SCREEN_WIDTH, SCREEN_HEIGHT) / columns;
+    squareRect.h = MIN(SCREEN_WIDTH, SCREEN_HEIGHT) / rows;
+
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < columns; j++) {
+            squareRect.x = j * squareRect.w;
+            squareRect.y = i * squareRect.h;
+        }
+    }
+}
+
+int Draw() {
     // Initialize SDL
-    if(SDL_Init(SDL_INIT_VIDEO) < 0)
-    {
-        printf("SDL could not be initialized!\n"
-               "SDL_Error: %s\n", SDL_GetError());
+    if (SDL_Init(SDL_INIT_EVERYTHING) < 0) {
+        printf(
+            "SDL could not be initialized!\n"
+            "SDL_Error: %s\n",
+            SDL_GetError());
         return 0;
     }
-
-#if defined linux && SDL_VERSION_ATLEAST(2, 0, 8)
-    // Disable compositor bypass
-    if(!SDL_SetHint(SDL_HINT_VIDEO_X11_NET_WM_BYPASS_COMPOSITOR, "0"))
-    {
-        printf("SDL can not disable compositor bypass!\n");
-        return 0;
-    }
-#endif
 
     // Create window
     SDL_Window *window = SDL_CreateWindow("Basic C SDL project",
@@ -38,73 +44,40 @@ int Draw(){
                                           SDL_WINDOWPOS_UNDEFINED,
                                           SCREEN_WIDTH, SCREEN_HEIGHT,
                                           SDL_WINDOW_SHOWN);
-    if(!window)
-    {
-        printf("Window could not be created!\n"
-               "SDL_Error: %s\n", SDL_GetError());
-    }
-    else
-    {
+    if (!window) {
+        printf(
+            "Window could not be created!\n"
+            "SDL_Error: %s\n",
+            SDL_GetError());
+    } else {
         // Create renderer
         SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-        if(!renderer)
-        {
-            printf("Renderer could not be created!\n"
-                   "SDL_Error: %s\n", SDL_GetError());
-        }
-        else
-        {
-            // Declare rect of square
-            SDL_Rect squareRect;
-
-            // Square dimensions: Half of the min(SCREEN_WIDTH, SCREEN_HEIGHT)
-            squareRect.w = MIN(SCREEN_WIDTH, SCREEN_HEIGHT) / 2;
-            squareRect.h = MIN(SCREEN_WIDTH, SCREEN_HEIGHT) / 2;
-
-            // Square position: In the middle of the screen
-            squareRect.x = SCREEN_WIDTH / 2 - squareRect.w / 2;
-            squareRect.y = SCREEN_HEIGHT / 2 - squareRect.h / 2;
-
-
-            // Event loop exit flag
+        if (!renderer) {
+            printf(
+                "Renderer could not be created!\n"
+                "SDL_Error: %s\n",
+                SDL_GetError());
+        } else {
             bool quit = false;
 
             // Event loop
-            while(!quit)
-            {
+            while (!quit) {
                 SDL_Event e;
 
                 // Wait indefinitely for the next available event
                 SDL_WaitEvent(&e);
 
                 // User requests quit
-                if(e.type == SDL_QUIT)
-                {
+                if (e.type == SDL_QUIT) {
                     quit = true;
                 }
-
-                // Initialize renderer color white for the background
-                SDL_SetRenderDrawColor(renderer, 0xFF, 0xFF, 0xFF, 0xFF);
-
-                // Clear screen
                 SDL_RenderClear(renderer);
-
-                // Set renderer color red to draw the square
-                SDL_SetRenderDrawColor(renderer, 0xFF, 0x00, 0x00, 0xFF);
-
-                // Draw filled square
-                SDL_RenderFillRect(renderer, &squareRect);
-
-                // Update screen
+                DrawGrid(10, 10, window, renderer);
                 SDL_RenderPresent(renderer);
             }
-
-            // Destroy renderer
             SDL_DestroyRenderer(renderer);
+            SDL_DestroyWindow(window);
         }
-
-        // Destroy window
-        SDL_DestroyWindow(window);
     }
 
     // Quit SDL
@@ -112,8 +85,3 @@ int Draw(){
 
     return 0;
 }
-// int main(int argc, char** argv)
-// {
-//     Draw();
-//     return 0;
-// }
--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,13 @@
+#include <SDL2/SDL.h>
+
 #include <iostream>
-#include <SDL2/SDL.h> 
+
 #include "Custom.h"
 
 using namespace std;
 
-int main(int argc, char* argv[]){
-    
-    cout << "Hi" << endl;
+int main(int argc, char* argv[]) {
     Draw();
-    cout << "Bye" << endl;
-    
+
     return 0;
 }
